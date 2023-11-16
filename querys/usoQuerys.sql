e7e13e5d7bc9f373affc8e6ce383cfcb6e17c6ca
Sergio <sergio.ayala9208@gmail.com>
2023-11-15 22:02:32 -0500

    mas querys

--- querys/usoQuerys.sql
+++ querys/usoQuerys.sql
@@ -7,8 +7,11 @@ select * from tabla_de_productos where SABOR= 'uva';
 select * from tabla_de_productos where SABOR= 'mango';
 select * from tabla_de_productos where ENVASE='lata';
 
+#uso de condicionales
 select * from tabla_de_productos where PRECIO_DE_LISTA >= 3 order by PRECIO_DE_LISTA desc;
 select * from tabla_de_productos where PRECIO_DE_LISTA >= 16 order by PRECIO_DE_LISTA desc;
+
+#Uso de condicionales and, or, in
 select * from tabla_de_productos where SABOR = 'mango' and TAMANO = '1,5 litros';
 select * from tabla_de_productos where not(SABOR = 'mango' or TAMANO = '1,5 litros');
 select * from tabla_de_productos where not(SABOR = 'mango') or TAMANO = '1,5 litros';
@@ -21,5 +24,15 @@ select * from tabla_de_clientes where edad between 12 and 25;
 select * from tabla_de_productos where SABOR like '%man%';
 select * from tabla_de_productos where SABOR like '%cereza';
 select * from tabla_de_productos where SABOR like '%go';
+
+# uso de distinct
+select envase, tamano from tabla_de_productos;
+select distinct envase, tamano from tabla_de_productos;
+select distinct envase, tamano, sabor from tabla_de_productos where sabor='naranja';
+
+#uso de limit
+select * from tabla_de_productos;
+select * from tabla_de_productos limit 5;
+
 
 
